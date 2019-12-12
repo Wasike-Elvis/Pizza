314e9c563ffa9cc7c38271ef0181ae7c3dcf97a4
Wasike-Elvis <wasikeelvis@gmail.com>
2019-12-12 13:09:58 +0300

    Pizza prices and hover working

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+    $(".card-1").hover(function(){
+        $(this).animate({opacity:'1'});
+    },
+    function(){4
+        $(this).animate({opacity:'0'});
+        });    
+})
+
+
